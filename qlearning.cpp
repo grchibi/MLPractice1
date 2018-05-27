780db1f4cb43a55156cfb4988f8394fc80e0534e
grchibi <tomoyukit2@gmail.com>
2018-05-27 19:57:37 +0900

    QLManagerクラスの新規追加。

---
+++ qlearning.cpp
@@ -0,0 +1,15 @@
+#include "qlearning.h"
+
+namespace QL
+{
+    QLManager::QLManager()
+    {
+
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ qlearning.h
@@ -0,0 +1,15 @@
+#ifndef _QLEARNING_H_
+#define _QLEARNING_H_
+
+namespace QL
+{
+    class QLManager
+    {
+    public:
+        QLManager(void);
+        ~QLManager() {}
+        
+    };
+}
+
+#endif  // _QLEARNING_H_
